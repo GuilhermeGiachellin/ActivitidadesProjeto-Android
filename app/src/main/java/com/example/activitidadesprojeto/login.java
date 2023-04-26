28c76a49bec384f9b7405bca27ab63b94f904af8
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2023-04-26 18:11:41 -0300

    Added filter for fail and success login

--- app/src/main/java/com/example/activitidadesprojeto/login.java
+++ app/src/main/java/com/example/activitidadesprojeto/login.java
@@ -2,7 +2,10 @@ package com.example.activitidadesprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void botaoLogin (View v) {
+        EditText loginEmail = (EditText) findViewById(R.id.loginEmail);
+        EditText loginSenha = (EditText) findViewById(R.id.loginSenha);
+
+        String emailString = loginEmail.getText().toString();
+        String senhaString = loginSenha.getText().toString();
+
+        String emailCadastrado = "gui@gmail.com";
+        String senhaCadastrada = "123456";
+
+        if(emailString.isEmpty() || senhaString.isEmpty()) {
+            Intent telaFalhaLogin = new Intent(this, falhaLogin.class);
+            startActivity(telaFalhaLogin);
+
+        } else if (emailString == emailCadastrado && senhaString == senhaCadastrada) {
+            Intent telaSucessoLogin = new Intent(this, sucessoLogin.class);
+            startActivity(telaSucessoLogin);
+
+        } else {
+            startActivity(telaFalhaLogin);
+        }
     }
 }
