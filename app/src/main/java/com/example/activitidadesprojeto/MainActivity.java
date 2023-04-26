1bb5b60beb79af4962bec95091b093eb00e00926
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2023-04-26 17:49:36 -0300

    Create login

--- app/src/main/java/com/example/activitidadesprojeto/MainActivity.java
+++ app/src/main/java/com/example/activitidadesprojeto/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.activitidadesprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void botaoLoginOnClick(View v) {
+        Intent telaLogin = new Intent(this, login.class);
+        startActivity(telaLogin);
+    }
+
 }
