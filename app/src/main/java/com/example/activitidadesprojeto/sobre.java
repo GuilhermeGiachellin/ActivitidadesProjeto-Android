11d9008b103febd18b486d55e999d4885bbf5a9c
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2023-04-27 11:07:05 -0300

    add uniftec link

--- app/src/main/java/com/example/activitidadesprojeto/sobre.java
+++ app/src/main/java/com/example/activitidadesprojeto/sobre.java
@@ -2,7 +2,10 @@ package com.example.activitidadesprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class sobre extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class sobre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+    }
+
+    public void uniftecOnClick (View v) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ftec.com.br")));
     }
 }
